ff2332e4f0587b7c28444473da47e9209255108a
aymanelsayeed <aymanelsayeed@gmail.com>
2025-12-12 12:15:18 +0200

    add doc

---
+++ src/src.py
@@ -0,0 +1,10 @@
+
+def sum_two_numbers (a: int, b: int):
+    """
+    Sum two numbers
+    :param a: the first number
+    :param b: the second number
+    :return: the sum of the two numbers
+    :rtype: int
+    """
+    return a + b
