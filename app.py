af97f5c0f62f11598db056db41e453f3a98032ff
aymanelsayeed <aymanelsayeed@gmail.com>
2025-12-19 14:51:52 +0200

    add app.

---
+++ app.py
@@ -0,0 +1,22 @@
+"""
+
+A simple FastAPI application.
+
+"""
+
+import asyncio
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/sleep/async-aio')
+async def aio_sleep():
+    await asyncio.sleep(1)
+    return {'error': None}
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000)
